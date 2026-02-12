77cb8e4fa0c160b4fe3a4a48adeb01d47fe79687
Sean Field <31040158+sfield2@users.noreply.github.com>
2026-02-12 09:00:58 -0700

    Initial commits and structure

--- master.R
+++ master.R
@@ -13,35 +13,41 @@ study_extent <- st_read("./DATA/study_extent.shp")%>%
   st_transform(.,crs=4269)
 
 ## read in raster data
-r <- terra::rast("./DATA/CLIMATE/PPT.tif")
+#r <- terra::rast("./DATA/CLIMATE/PPT.tif")
 r_var <- terra::rast("./DATA/CLIMATE/PPT_annualvariability.tif")
 
-#clipr <- ext(c(xmin=-107.6,xmax=-107.45,ymin=35.25,ymax=35.4))
-#clipr <- ext(c(xmin=-10,xmax=-107,ymin=35,ymax=36))
-#r <- crop(r,clipr)
 
 ## calculate changes year to year
 # r_var <- (subset(r, 1:(nlyr(r)-1)) - subset(r, 2:nlyr(r))) * -1
-# writeRaster(r,"./DATA/CLIMATE/PPT_annualvariability.tif")
+# writeRaster(r_var,"./DATA/CLIMATE/PPT_annualvariability.tif")
 
-# r_var1 <- r_var[[600:1300]]
-# 
-# df <- as.data.frame(r_var1,xy=T)%>%
-#   pivot_longer(cols = -c(x, y), names_to = "layer",values_to = "value")%>%
-#   mutate(layer_num = as.numeric(gsub("PPT_", "", layer)))
-# 
-# p <- ggplot(df, aes(x = x, y = y, fill = value)) +
-#   geom_raster() +
-#   scale_fill_gradientn(colors=c("#bb3e03","#ca6702","#ee9b00","#FEFAE040","#94d2bd","#0a9396","#005f73"),rescaler = ~ scales::rescale_mid(.x, mid = 0))+
-#   coord_equal() +
-#   theme_minimal() +
-#   labs(title = "Time Step: {frame_time}") +
-#   transition_time(layer_num) +
-#   ease_aes("linear")
-# 
-# animate(p, fps = 5,nframes = 10, width = 600, height = 600)
-# 
-# anim_save("./FIGURES/raster_precipvariability_600to1300.gif")
+#clipr <- ext(c(xmin=-107.6,xmax=-107.45,ymin=35.25,ymax=35.4))
+clipr <- ext(c(xmin=-108,xmax=-107,ymin=35,ymax=36))
+r_var <- crop(r_var,clipr)
+
+r_var1 <- r_var[[6:13]]
+
+df <- as.data.frame(r_var1,xy=T)%>%
+  pivot_longer(cols = -c(x, y), names_to = "layer",values_to = "value")%>%
+  mutate(layer_num = as.numeric(gsub("PPT_", "", layer)))
+
+p <- ggplot(df, aes(x = x, y = y, fill = value)) +
+  geom_raster() +
+  scale_fill_gradientn(colors=c("#bb3e03","#ca6702","#ee9b00","#FEFAE040","#94d2bd","#0a9396","#005f73"),
+                       rescaler = ~ scales::rescale_mid(.x, mid = 0),
+                       name = "Precipitation 
+Variability (mm)")+
+  #coord_equal() +
+  labs(title = "Year (CE): {frame_time}",
+       x=NULL,
+       y=NULL) +
+  transition_time(layer_num) +
+  ease_aes("linear")+
+  theme_minimal()
+
+animate(p, fps = 1,nframes = 7, width = 750, height = 600)
+
+anim_save("./FIGURES/raster_precipvariability_600to1300.gif")
 
 
 
@@ -90,14 +96,66 @@ gh <- merge(chaco_greathouses,outlier_database,by="SITE_NA")%>%
   filter(!is.na(END_DATE))%>%
   subset(BEGIN_DATE != "No Data")%>%
   subset(END_DATE != "No Data")%>%
-  select(-c("SITE_NUMBE.x","SITE_NUM_1","PROJECTED_","PROJECTE_1","PROJECTE_2"))
+  select(-c("SITE_NUMBE.x","SITE_NUM_1","PROJECTED_","PROJECTE_1","PROJECTE_2"))%>%
+  mutate(END_DATE = as.numeric(END_DATE),
+         BEGIN_DATE = as.numeric(BEGIN_DATE),
+         occupation_length = END_DATE - BEGIN_DATE)%>%
+  subset(occupation_length >= 1)
+
+rm(chaco_greathouses,outlier_database)
+
+
+### 
+
+
+
 
 
 ### Read in VEP II North site locations
 vepii_sites<-read.csv("C:/Users/sfield3/Documents/RESEARCH/DATA GENERAL/SENSITIVE SITE LOCATIONS/db_vepii_n.csv")%>%
   st_as_sf(.,coords=c("UTMEast","UTMNorth"),crs=26912)%>%
   st_transform(.,crs=4269)%>%
-  st_intersection(.,study_extent)
+  st_intersection(.,study_extent)%>%
+  mutate(UNIQ_ID = 1:nrow(.))
+
+vep_df <- read.csv("C:/Users/sfield3/Documents/RESEARCH/DATA GENERAL/SENSITIVE SITE LOCATIONS/db_vepii_n.csv")%>%
+  select(c("ph6","ph7","ph8","ph9","ph10","ph11","ph12",
+           "ph13","ph14","ph15","ph16","ph17","ph18","ph19"))%>%
+  mutate(UNIQ_ID = 1:nrow(.))%>%
+  gather(phase,value,-UNIQ_ID)%>%
+  mutate(value = ifelse(value<0,0,value))
+
+vep_dates <- read.csv("./DATA/VEP_dates.csv")
+
+vep <- left_join(vep_df,vep_dates,c("phase"="PHASE"))
+
+rm(vep_dates)
+
+vep<- arrange(vep,desc(UNIQ_ID),BEGIN_D)
+
+expanded_vep <- vep %>%
+  rowwise() %>%
+  mutate(year = list(BEGIN_D:END_DAT)) %>%
+  unnest(year) %>%
+  select(UNIQ_ID, year, value) 
+
+vep_years <- expanded_vep%>%
+  group_by(UNIQ_ID)%>%
+  mutate(year_until = find_years_until_disoccupation(value))%>%
+  setNames(c("site_name","Year","occupation","years_until_disoccupation"))
+
+length(unique(vep_years$site_name))
+
+
+vep_detail <- merge(vep_years,vep_df,by=c("site_name","Year"))
+
+vep_detail <- distinct(vep_detail)
+
+
+
+
+
+
 
 ### Read in Dendrochronological data 
 # data shared by Tim on TDAR 3/26/2025
@@ -145,98 +203,6 @@ find_years_until_disoccupation <- function(x) {
   out
 }
 
-
-############# GREAT HOUSE STUFF #####################
-gh_output <- read.csv("./OUTPUT/summaries/chaco_greathouses/chaco_greathouse_1.csv")%>%
-  #mutate(Mean_precip_variab = abs(Mean_precip_variab))%>%
-  select(-c("X"))
-
-gh <- st_read("./OUTPUT/spatial_outputs/chaco_greathouses.shp")%>%
-  as.data.frame()%>%
-  mutate(END_DAT = as.numeric(END_DAT))%>%
-  mutate(BEGIN_D = as.numeric(BEGIN_D))%>%
-  mutate(occupation_length = END_DAT - BEGIN_D)%>%
-  subset(occupation_length >= 1)
-
-
-
-########## look at variability in years before end of occupation
-for (j in 1:nrow(gh)){
-  start_year <- as.numeric(gh[j,c("BEGIN_D")])
-  end_year <- as.numeric(gh[j,c("END_DAT")])
-  site_na <- gh[j,c("UNIQU")]
-  
-  if(j==1){
-    years <- as.data.frame(start_year:end_year)%>%
-      mutate(site_name = site_na)%>%
-      set_colnames(c("Year","site_name"))%>%
-      mutate(years_before_abandonment = end_year-Year)
-  }else {
-    years_new <- as.data.frame(start_year:end_year)%>%
-      mutate(site_name = site_na)%>%
-      set_colnames(c("Year","site_name"))%>%
-      mutate(years_before_abandonment = end_year-Year)
-    years<-rbind(years,years_new)
-  }
-}
-rm(years_new)
-
-gh_summary <- merge(gh_output,years,by=c("Year","site_name"))
-
-write.csv(gh_summary,"./OUTPUT/summaries/chaco_greathouses/gh_detail.csv")
-
-
-
-
-################### VEP STUFF ###############################
-#### bind all data together
-for(i in 1:10){
-  df<-read.csv(paste("./OUTPUT/summaries/vep/vep_",i,".csv",sep=""))
-  name<-paste("vep_",i,sep="")
-  assign(name,df)
-}
-
-vep_df <- rbind(vep_1,vep_2,vep_3,vep_4,vep_5,vep_6,vep_7,
-                vep_8,vep_9,vep_10)%>%
-  select(-c("X"))
-
-
-#write.csv(vep_df,"./OUTPUT/summaries/vep/vep_total_recon.csv")
-
-vep <- st_read("./OUTPUT/spatial_outputs/vepii_north_data.shp")%>%
-  as.data.frame()%>%
-  select(c("UNIQU","ph6","ph7","ph8","ph9","ph10","ph11","ph12",
-           "ph13","ph14","ph15","ph16","ph17","ph18","ph19"))%>%
-  gather(phase,value,-UNIQU)%>%
-  mutate(value = ifelse(value<0,0,value))
-
-vep_dates <- read.csv("./DATA/SETTLEMENT/VEP_dates.csv")
-
-vep <- left_join(vep,vep_dates,c("phase"="PHASE"))
-
-rm(vep_dates)
-
-vep<- arrange(vep,desc(UNIQU),BEGIN_D)
-
-expanded_vep <- vep %>%
-  rowwise() %>%
-  mutate(year = list(BEGIN_D:END_DAT)) %>%
-  unnest(year) %>%
-  select(UNIQU, year, value) 
-
-vep_years <- expanded_vep%>%
-  group_by(UNIQU)%>%
-  mutate(year_until = find_years_until_disoccupation(value))%>%
-  set_colnames(c("site_name","Year","occupation","years_until_disoccupation"))
-
-length(unique(vep_years$site_name))
-
-
-vep_detail <- merge(vep_years,vep_df,by=c("site_name","Year"))
-
-vep_detail <- distinct(vep_detail)
-
-write.csv(vep_detail,"./OUTPUT/summaries/vep/vep_detail.csv")
 
 
 
